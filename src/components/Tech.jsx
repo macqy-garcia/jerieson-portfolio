447f9d6f30c1aabbd9ccdcfa05c57653cab77d8a
jerieson <jeriesonasis4@gmail.com>
2024-10-16 17:24:25 +0800

    initial commit

---
+++ src/components/Tech.jsx
@@ -0,0 +1,29 @@
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motion";
+import { styles } from "../styles";
+
+const Tech = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider mb-10">
+          My Techstack
+        </p>
+        {/* <h2 className={styles.sectionHeadText}>Work Experience.</h2> */}
+      </motion.div>
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Tech, "");
---
+++ src/constants/index.js
@@ -0,0 +1,265 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  promptopia,
+  threejs,
+  carepulse,
+  mern,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "UI/UX Designer",
+    icon: mobile,
+  },
+  {
+    title: "Frontend Developer",
+    icon: backend,
+  },
+  {
+    title: "Backend Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
+];
+
+const experiences = [
+  {
+    title: "Software Engineer",
+    company_name: "Macqy Garcia Consultancy",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "Sept 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "React Native Developer",
+    company_name: "Tesla",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "Jan 2021 - Feb 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Shopify",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Jan 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "Meta",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Carepulse",
+    description:
+      "Built with Next.js, TypeScript, and TailwindCSS, streamlines healthcare appointments with secure patient registration, intuitive booking, and SMS notifications via Twilio. And appwrite that supports backend services for secure file storage. All these components work together to create a seamless and efficient healthcare appointment experience.",
+    tags: [
+      {
+        name: "next.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "appwrite",
+        color: "green-text-gradient",
+      },
+      {
+        name: "shadcn-ui",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carepulse,
+    source_code_link: "https://github.com/jerieson/jerieson_carepulse",
+  },
+  {
+    name: "Promptopia",
+    description:
+      "Built with Next.js, leverages server-side rendering (SSR) and static site generation (SSG) for performance and SEO. It features a responsive UI for creating and sharing AI prompts, a CRUD system for managing prompts, and Google Sign-in via NextAuth. The robust backend is powered by Next.js API routes.",
+    tags: [
+      {
+        name: "next.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: promptopia,
+    source_code_link: "https://github.com/jerieson/project_promptopia",
+  },
+  {
+    name: "GoalSetter App",
+    description:
+      "Built with the MERN stack, provides secure user authentication with JWT tokens and efficient goal management, allowing users to create and delete time-specific goals. It leverages Redux DevTools for state management and Postman for API testing, ensuring a seamless and reliable user experience.",
+    tags: [
+      {
+        name: "react-redux",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "express.js",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: mern,
+    source_code_link: "https://github.com/jerieson/goal-setter-app",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
